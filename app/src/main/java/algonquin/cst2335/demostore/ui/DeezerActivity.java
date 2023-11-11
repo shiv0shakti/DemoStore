14b560524f1309058e105821644c212371655b6d
Rishabh Puri <puri0023@algonquinlive.com>
2023-11-11 07:08:58 -0500

    Second Commit: Different Ui for all Apps

---
+++ app/src/main/java/algonquin/cst2335/demostore/ui/DeezerActivity.java
@@ -0,0 +1,17 @@
+package algonquin.cst2335.demostore.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import algonquin.cst2335.demostore.R;
+
+public class DeezerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deezer);
+    }
+}
---
+++ app/src/main/java/algonquin/cst2335/demostore/ui/RecipeActivity.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.demostore.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import algonquin.cst2335.demostore.R;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}

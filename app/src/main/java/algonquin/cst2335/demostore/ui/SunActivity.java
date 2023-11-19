546b39be40cc2918af11598f7b069f7c960e8cdf
roy sullivan <10569633+roy-a-sullivan@users.noreply.github.com>
2023-11-18 22:05:37 -0500

    Added interface, shared prefs, and translations.

--- app/src/main/java/algonquin/cst2335/demostore/ui/SunActivity.java
+++ app/src/main/java/algonquin/cst2335/demostore/ui/SunActivity.java
@@ -1,16 +1,75 @@
 package algonquin.cst2335.demostore.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import algonquin.cst2335.demostore.R;
+import algonquin.cst2335.demostore.databinding.ActivitySunBinding;
 
 public class SunActivity extends AppCompatActivity {
+
+    private static final String TAG = SunActivity.class.getName();
+    private ActivitySunBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sun);
+
+        binding = ActivitySunBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.sunsetMainToolbar);
+
+        SharedPreferences preferences = getSharedPreferences("SunsetData", Context.MODE_PRIVATE);
+        String latData = preferences.getString("Latitude", "");
+        String longData = preferences.getString("Longitude", "");
+
+        Button searchBtn = binding.btnSearch;
+        EditText latField = binding.latText;
+        EditText longField = binding.longText;
+
+        latField.setText(latData);
+        longField.setText(longData);
+
+        searchBtn.setOnClickListener(click -> {
+            SharedPreferences.Editor editor = preferences.edit();
+            String latText = latField.getText().toString();
+            String longText = longField.getText().toString();
+
+            editor.putString("Latitude", latText);
+            editor.putString("Longitude", longText);
+            editor.apply();
+
+            if (!longText.isEmpty() && !latText.isEmpty()) {
+                // do stuff...
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.sunset_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int choice = item.getItemId();
+
+        if (choice == R.id.sunsetHelp) {
+
+        } else if (choice == R.id.sunsetFavs) {
+
+        } else if (choice == R.id.sunsetMakeFav) {
+
+        }
+        return true;
     }
 }
